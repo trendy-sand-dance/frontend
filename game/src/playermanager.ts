e51c4ac33dee2c2bd943252b5448af0d462cdf68
fvan-wij <fvan-wij@student.codam.nl>
2025-05-22 21:25:47 +0200

    Implemented player stats when clicking on a player

--- game/src/playermanager.ts
+++ game/src/playermanager.ts
@@ -77,22 +77,68 @@ class PlayerManager {
         playerInfoBox.style.left = `${mouse.x + 10}px`;
 
         try {
+
+          const response = await fetch(`/game/userinfo/${id}`);
+          const responseFriends = await fetch(`/areFriends/${id}/${window.__USER_ID__}`);
+          const friendStatus = await responseFriends.json();
+          const { user } = await response.json() as { user: User };
+
           const infoUsername = document.getElementById("infoUsername");
           const infoAvatar = document.getElementById("infoAvatar");
+
+          const stats = document.getElementById("winsAndLosses");
+          const status = document.getElementById("status");
+          const friends = document.getElementById("friends");
+
+          if (stats && status && friends) {
+
+            stats.textContent = `Wins: ${user.wins} | Losses: ${user.losses}`;
+            if (user.status)
+              status.textContent = `Online`;
+            else
+              status.textContent = `Offline`;
+
+            if (friendStatus.status !== null) {
+              friends.textContent = `Friends: ${friendStatus.status}`;
+            }
+            else {
+              friends.textContent = ``;
+            }
+
+          }
+
+          console.log("friendStatus", friendStatus);
+
           if (infoUsername)
             infoUsername.textContent = `${username}`;
           if (infoAvatar)
             infoAvatar.outerHTML = `<img src="/images/avatars/${avatar}" class="w-12 h-12 rounded-full" />`;
 
           // Set up friend request button
-          const friendReqBtn = document.getElementById("friendRequestBtn");
-          if (friendReqBtn) {
-            friendReqBtn.setAttribute("hx-post", `sendReq/${id}/${window.__INITIAL_STATE__.id}`);
-            friendReqBtn.setAttribute("hx-target", "#friendRequestBtn");
-            friendReqBtn.setAttribute("hx-swap", "outerHTML");
-            friendReqBtn.setAttribute("hx-target-error", "#friendRequestBtn");
+          // const friendReqBtn = document.getElementById("friendRequestBtn");
+          const friendReq = document.getElementById("friendRequest");
+
+          if (friendReq && friendStatus.friend == false) {
+            friendReq.style.display = "block";
+            // Friend Req Btn
+            const isBtnThere = document.getElementById('friendReqBtn');
+            if (!isBtnThere) {
+              const btn = document.createElement('button');
+              btn.setAttribute("id", "friendReqBtn");
+              btn.setAttribute("type", "button");
+              btn.setAttribute("class", "bg-[#FF55FE] text-white rounded-md p-2");
+              btn.setAttribute("hx-post", `sendReq/${id}/${window.__USER_ID__}`);
+              btn.innerHTML = "Send friend request";
+              friendReq.appendChild(btn);
+            }
+            else if (isBtnThere) {
+              isBtnThere.innerHTML = "Send friend request";
+            }
 
             window.htmx.process(document.body);
+          }
+          else if (friendReq && friendStatus.friend) {
+            friendReq.style.display = "none";
           }
 
         } catch (err) {
