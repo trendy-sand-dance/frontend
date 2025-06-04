053a34ccae30358786202ba1c54577a6066bc01f
joppe <jboeve009@gmail.com>
2025-06-05 01:34:02 +0200

    Add oauth stuff

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import  Fastify, { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
-import { JWT_SECRET, PORT, ADDRESS, LOCAL_GAMESERVER_URL, USERMANAGEMENT_URL } from './config';
+import { JWT_SECRET, PORT, ADDRESS, LOCAL_GAMESERVER_URL, USERMANAGEMENT_URL, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from './config';
 
 import { routes } from './routes/routes.js';
 import closeWithGrace from 'close-with-grace';
@@ -12,6 +12,7 @@ import pluginView from '@fastify/view';
 import pluginMultipart from '@fastify/multipart';
 import pluginJwt, { FastifyJWT } from '@fastify/jwt'
 import pluginCookie from '@fastify/cookie'
+import OAuth2, {OAuth2Namespace} from "@fastify/oauth2";
 
 
 import { FastifyStaticOptions } from '@fastify/static';
@@ -22,6 +23,8 @@ import path from 'node:path';
 // import fs from 'fs';
 // const key =  path.join(path.dirname(__dirname), './server/server.key');
 // const cert = path.join(path.dirname(__dirname), './server/server.crt');
+import { googleOAuth2Routes } from './controllers/oauth2/google.controller';
+
 
 const fastify: FastifyInstance = Fastify({
   logger: {
@@ -47,8 +50,16 @@ fastify.register(pluginMultipart), {
 
 fastify.register(pluginFormbody);
 
+const corsOptions = {
+    // Allow all origins
+    // VERY IMPORTANT: In response, the server returns an Access-Control-Allow-Origin header with Access-Control-Allow-Origin: *
+    // which means that the resource can be accessed by any origin. (VERY DANGER!)
+    // You can read more about in:
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    origin: "*"
+}
 
-fastify.register(pluginCORS), {
+fastify.register(pluginCORS, corsOptions), {
   origin: true, // Specify domains for production
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
@@ -104,8 +115,52 @@ fastify.decorate(
 	},
 )
 
+// Google OAuth2 Options
+const googleOAuth2Options = {
+    // Namespace
+    name: 'GoogleOAuth2',
+    // Scopes
+    scope: ['profile', 'email'],
+    credentials: {
+        client: {
+            // Put in your client id here
+            id: GOOGLE_CLIENT_ID,
+            // Put in your client secret here
+            secret: GOOGLE_CLIENT_SECRET
+        },
+        // @fastify/oauth2 google provider
+        auth: OAuth2.GOOGLE_CONFIGURATION
+    },
+    // This option will create a new root with the GET method to log in through Google.
+    // Make sure you don't have any other routes in this path with the GET method.
+    startRedirectPath: '/oauth2/google',
+    // Here you specify the Google callback route.
+    // All logics will be checked after the success login or failure login in this path.
+    callbackUri: `http://localhost:8000/oauth2/google/callback`,
+    // The following 2 functions are check in detail whether the input parameters from Google include the state query parameter or not
+
+		// TODO: Make this shit work
+
+    // generateStateFunction: (request: FastifyRequest, reply: FastifyReply) => {
+    // reply == reply;
+    //     // @ts-ignore
+    //     return request.query.state
+    // },
+    // checkStateFunction: (request: FastifyRequest, callback: any) => {
+    //     // @ts-ignore
+    //     if (request.query.state) {
+    //         callback()
+    //         return;
+    //     }
+    //     callback(new Error('Invalid state'))
+    // }
+};
+
+fastify.register(OAuth2, googleOAuth2Options)
 
 fastify.register(routes);
+
+fastify.register(googleOAuth2Routes, { prefix: "/oauth2" });
 
 async function startServer() {
   closeWithGrace(
