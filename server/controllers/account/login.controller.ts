01cdd1966f7d4763294c08a231f4619c9e52fafa
joppe <jboeve009@gmail.com>
2025-06-15 00:01:01 +0200

    Store wether the user is a google user in the `window` for global state, we get this value from the getUser endpoint in the db

--- server/controllers/account/login.controller.ts
+++ server/controllers/account/login.controller.ts
@@ -1,6 +1,6 @@
-import { FastifyRequest, FastifyReply } from 'fastify';
+import { FastifyReply, FastifyRequest } from 'fastify';
 // import { User } from '../../types';
-import { USERMANAGEMENT_URL, DATABASE_URL } from '../../config';
+import { DATABASE_URL, USERMANAGEMENT_URL } from '../../config';
 
 
 export async function getLoginView(request: FastifyRequest, reply: FastifyReply) {
